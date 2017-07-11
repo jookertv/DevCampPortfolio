911072cccbc3144f1890aade24f6b742cbd1cc5c
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 14:44:20 +0000

    Implemented gritter for notifications

--- DevCampPortfolioTwo/app/controllers/portfolios_controller.rb
+++ DevCampPortfolioTwo/app/controllers/portfolios_controller.rb
@@ -20,7 +20,6 @@ class PortfoliosController < ApplicationController
   
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
   end  
    access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
   def create
@@ -69,7 +68,7 @@ class PortfoliosController < ApplicationController
                                     :body,
                                     :main_image,
                                     :thumb_image,
-                                    technologies_attributes: [:name]
+                                    technologies_attributes: [:id, :name, :_destroy]
                                     )
   end
   
