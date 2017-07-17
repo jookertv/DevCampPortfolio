b4552e02919596f9f0078567dbde5362bc4bc04c
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 16:19:29 +0000

    Added validations for topics in blogs and added a topic form element

--- DevCampPortfolioTwo/app/controllers/blogs_controller.rb
+++ DevCampPortfolioTwo/app/controllers/blogs_controller.rb
@@ -15,7 +15,7 @@ class BlogsController < ApplicationController
 
 
   def show
-    if logged in?(:site_admin) || @blog.published?
+    if logged_in?(:site_admin) || @blog.published?
       @blog = Blog.includes(:comments).friendly.find(params[:id])
       @comment = Comment.new
       
@@ -90,7 +90,7 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
 end
 
